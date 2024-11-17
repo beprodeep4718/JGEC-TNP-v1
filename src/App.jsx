da2b4f9604f56c3226c5171bbf8b2d5fe5ceefbd
Beprodeep Das <codingbtech@gmail.com>
2024-11-17 12:45:25 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Team from "./pages/Team";
+import Home from "./pages/Home";
+import Recruiter from "./pages/Recruiter";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/recruiter" element={<Recruiter />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
